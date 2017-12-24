c75207189b21f35862491ed19e75270c8e070974
NoSinging <NoSinging@users.noreply.github.com>
2017-12-24 16:11:13 +1100

    add linear regression model make predictions visualise key features and predictions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # machine-learn-boston-house
 A demo of basic machine learning using the boston house price data
+
+with thanks to:
+https://egghead.io/lessons/scikit-learn-use-linear-regression-to-estimate-continuous-values-with-python-and-scikit-learn
+
+
--- boston.py
+++ boston.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
@@ -5,12 +6,42 @@ from sklearn.linear_model import LinearRegression
 
 boston = datasets.load_boston()
 
-x = boston.data
+
+# print boston.keys()
+print boston.feature_names
+print boston.DESCR
+# print boston.data[:5]
+# print boston.target[:5]
+# print boston.data.shape
+# print boston.target.shape
+
+X = boston.data
 y = boston.target
 
-print boston.feature_names
-print x
-print y
+# visualise relationship between value and key features
+# plt.scatter(y, X[:,5])
+# plt.xlabel("rooms")
+# plt.ylabel("Prices")
+# plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1 )
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+# visualise relationship between Actual and predictions
+# plt.scatter(y_test, predictions)
+# plt.xlabel("Actual Prices")
+# plt.ylabel("Predicted Prices")
+# plt.show()
+
+# r square score
+print model.score(X_test, y_test)
+
+# mean squared error
+print metrics.mean_squared_error(y_test, predictions)
 
 exit()
 
